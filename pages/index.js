7a61e3d583b3468d2b967520da5d2e67fc5d5a47
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-09-01 11:34:07 +0200

    init next app

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Majornas böcker & kaffe</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>firstpage</h1>
+    </div>
+  );
+}
