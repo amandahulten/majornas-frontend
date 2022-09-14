6711c6365a40e1f99d2a4920284c3c94bd2cf5c1
s0wie <sowieh@gmail.com>
2022-09-14 21:15:38 +0200

    added background

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="h-[20px] bg-black"></div>
+      <div className="bg-[url('../public/bg-footer-desktop.svg')]">
+        <h1 className="text-[32px] text-white text-center">
+          Majorna bokhandel & kaffe
+        </h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
