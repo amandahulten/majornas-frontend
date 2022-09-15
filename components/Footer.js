7bc68f2953dd94d3a0917eb0d88872f24d1ebfd7
s0wie <sowieh@gmail.com>
2022-09-15 12:12:30 +0200

    Added logos

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer>
@@ -10,6 +12,28 @@ const Footer = () => {
           <p className="text-[32px] text-whiteish text-center font-h1">
             bokhandel & kaffe
           </p>
+          <div className="flex justify-center w-full mt-[40px]">
+            <div className="flex justify-between w-[160px]">
+              <div className="text-center">
+                <Image
+                  src="/logo-insta.svg"
+                  alt="instagram-logo"
+                  height="40"
+                  width="40"
+                />
+                <p className="text-[14px] text-whiteish">Instagram</p>
+              </div>
+              <div className="text-center">
+                <Image
+                  src="/logo-fb.svg"
+                  alt="instagram-logo"
+                  height="40"
+                  width="40"
+                />
+                <p className="text-[14px] text-whiteish">Facebook</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
