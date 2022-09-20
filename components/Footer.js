a47b6c1a9a4a2bdbd386ddb39e704b9a812d980d
s0wie <sowieh@gmail.com>
2022-09-20 16:37:47 +0200

    tablet done

--- components/Footer.js
+++ components/Footer.js
@@ -5,48 +5,52 @@ const Footer = () => {
     <footer>
       <div className="h-[20px] bg-black-coffee"></div>
       <div className="bg-[url('../public/bg-footer-desktop.svg')]">
-        <div className="py-2 px-8">
-          <p className="text-[32px] text-whiteish text-center font-h1">
-            majorna
-          </p>
-          <p className="text-[32px] text-whiteish text-center font-h1">
-            bokhandel & kaffe
-          </p>
-          <div className="flex justify-center w-full mt-[40px]">
-            <div className="flex justify-between w-[160px]">
-              <div className="text-center">
-                <Image
-                  src="/logo-insta.svg"
-                  alt="instagram-logo"
-                  height="40"
-                  width="40"
-                />
-                <p className="text-[14px] text-whiteish">Instagram</p>
-              </div>
-              <div className="text-center">
-                <Image
-                  src="/logo-fb.svg"
-                  alt="instagram-logo"
-                  height="40"
-                  width="40"
-                />
-                <p className="text-[14px] text-whiteish">Facebook</p>
+        <div className="py-2 px-8 md:flex md:justify-between md:pt-[50px]">
+          <div>
+            <p className="text-[32px] text-whiteish text-center font-h1">
+              majorna
+            </p>
+            <p className="text-[32px] text-whiteish text-center font-h1">
+              bokhandel & kaffe
+            </p>
+            <div className="flex justify-center w-full mt-[40px]">
+              <div className="flex justify-between w-[160px]">
+                <div className="text-center">
+                  <Image
+                    src="/logo-insta.svg"
+                    alt="instagram-logo"
+                    height="40"
+                    width="40"
+                  />
+                  <p className="text-[14px] text-whiteish">Instagram</p>
+                </div>
+                <div className="text-center">
+                  <Image
+                    src="/logo-fb.svg"
+                    alt="instagram-logo"
+                    height="40"
+                    width="40"
+                  />
+                  <p className="text-[14px] text-whiteish">Facebook</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="text-whiteish mt-[50px]">
-            <p>Tis - Fre</p>
-            <p className="font-bold">11:00 - 18:00</p>
-            <p className="mt-[18px]">Lör</p>
-            <p className="font-bold">11:00 - 15:00</p>
-            <p className="mt-[18px]">Sön - Mån</p>
-            <p className="font-bold">Stängt</p>
-          </div>
-          <div className="text-whiteish mt-[50px]">
-            <p className="mb-[18px]">Hem</p>
-            <p className="mb-[18px]">Aktuellt</p>
-            <p className="mb-[18px]">Om oss</p>
-            <p className="mb-[50px]">Kontakt</p>
+          <div className="md:flex md:flex-row-reverse w-[40%] md:justify-between md:mb-[50px]">
+            <div className="text-whiteish mt-[50px]  mr-[20px]">
+              <p>Tis - Fre</p>
+              <p className="font-bold">11:00 - 18:00</p>
+              <p className="mt-[18px]">Lör</p>
+              <p className="font-bold">11:00 - 15:00</p>
+              <p className="mt-[18px]">Sön - Mån</p>
+              <p className="font-bold">Stängt</p>
+            </div>
+            <div className="text-whiteish mt-[50px]">
+              <p className="mb-[18px]">Hem</p>
+              <p className="mb-[18px]">Aktuellt</p>
+              <p className="mb-[18px]">Om oss</p>
+              <p className="mb-[50px]">Kontakt</p>
+            </div>
           </div>
         </div>
       </div>
