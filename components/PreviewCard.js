0fd6ec735e53ae8ffe0a218ca9a207f151f982fc
Neo Lejondahl <neo.leijondahl@gmail.com>
2022-09-21 15:38:16 +0200

    Complete i think

--- components/PreviewCard.js
+++ components/PreviewCard.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 import headshot from "../assets/headshot.jpg";
 import RightArrowIcon from "../assets/icons/RightArrowIcon";
 
-const PreviewCard = ({ name, text }) => {
+const PreviewCard = ({ author, body, slug }) => {
   return (
     <div className=" max-w-full group flex flex-col relative text-black-coffee">
       <div className="aspect-square relative">
         <Image src={headshot} layout="fill" objectFit="cover" />
       </div>
 
-      <div className=" p-2 bg-temp-ey group-hover:bg-tumbleweed absolute w-full bottom-0">
-        <h3 className="text-2xl font-bold">{name}</h3>
-        <div className="h-0 text-sm group-hover:h-24 transition-all overflow-hidden">
-          <p>{text}</p>
-          <Link href="/gay">
-            <a className="mt-auto flex gap-2 w-full justify-end items-center">
+      <div className=" p-2 bg-temp-ey group-hover:bg-tumbleweed md:absolute w-full bottom-0">
+        <h3 className="text-2xl font-bold">{author}</h3>
+        <div className="h-fit text-sm md:h-0 group-hover:h-24 transition-all overflow-hidden min-h-fit">
+          <p className="mt-2">{body}</p>
+          <Link href={slug}>
+            <a className="mt-auto flex gap-2 w-full justify-end items-center h-full">
               <p className="text-xl font-courier-prime font-bold underline">
                 Läs mer
               </p>
