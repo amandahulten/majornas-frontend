ffc9090c263741114ff62b3676d5cf29fe84e8d3
amandahulten <amahul1029@skola.goteborg.se>
2022-09-26 11:46:03 +0200

    Added styling and icons

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import Openings from "../components/Openings";
 import Link from "next/link";
 import Image from "next/image";
 import Arrow from "../components/svgs/Arrow";
-import store from "../public/store.png";
-import boktips from "../public/boktips.png";
+import Coffee from "../components/svgs/Coffee";
 
 const Home = () => {
   return (
@@ -15,28 +14,42 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="pb-4 sm:px-20 md:px-0">
-        <h1 className="text-4xl md:text-5xl font-bold font-['Courier-prime']">
+        <h1 className="text-4xl py-4 md:text-5xl font-bold font-['Courier-prime']">
           OBEROENDE BOKHANDEL <br /> MED KAFFEHÄNG
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-4 lg:grid-rows-3 gap-4">
           <div className="relative md:col-span-4 md:row-span-2 lg:col-span-2">
-            <Image src={store} alt="Hejsan" layout="fill" objectFit="cover" />
+            <Image
+              src="/store.png"
+              alt="Hejsan"
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
 
           <Openings className="md:col-span-3 lg:col-span-1 md:row-start-3 md:col-start-4 lg:col-start-1" />
           <Contact className="md:col-span-3 lg:col-span-1 md:row-start-3 md:col-start-1 lg:col-start-2" />
 
-          <div className="bg-tumbleweed p-4 md:row-start-1 md:col-span-2 md:col-start-5 lg:col-start-3 lg:col-span-1">
-            <p className="text-5xl mb-10 font-['DM-serif'] text-[#4E4247] max-h-60 md:hidden lg:block">
+          <div className="bg-tumbleweed p-4 pb-0 md:row-start-1 md:col-span-2 md:col-start-5 lg:col-start-3 lg:col-span-1">
+            <p className="text-5xl mb-6 font-['DM-serif'] text-[#4E4247] max-h-60 md:hidden lg:block lg:text-4xl xl:hidden">
               POCKET + <br /> KAFFE = <br /> 100 kr
             </p>
-            <p className="hidden md:block lg:hidden text-5xl mb-5 font-['DM-serif'] text-[#4E4247] max-h-60">
-              POCKET <br /> + KAFFE <br /> = 100 kr
+
+            <p className="hidden md:block lg:hidden text-5xl md:text-3xl mb-5 font-['DM-serif'] text-[#4E4247] max-h-60">
+              POCKET + <br /> KAFFE = 100 kr
             </p>
+
+            <p className="hidden xl:block text-4xl mb-5 font-['DM-serif'] text-[#4E4247]">
+              POCKET + KAFFE <br /> = 100 kr
+            </p>
+
+            <div className="flex justify-center">
+              <Coffee width="230" />
+            </div>
           </div>
 
-          <div className="bg-temp-ey p-4 md:row-start-2 lg:row-start-1 md:col-span-2 md:col-start-5 lg:col-start-4 lg:col-span-1">
+          <div className="bg-temp-ey lg:flex flex-col justify-between p-4 md:row-start-2 lg:row-start-1 md:col-span-2 md:col-start-5 lg:col-start-4 lg:col-span-1">
             <p className="text-xl font-semibold mb-4">
               Ta ett kik på vad som försiggår i butiken. Vi planerar event
               kontinuerligt!
@@ -53,7 +66,12 @@ const Home = () => {
 
           <div className="bg-black-coffee md:col-span-6 lg:col-span-2 lg:row-span-2 lg:col-start-3 lg:row-start-2 md:flex flex-row-reverse">
             <div className="relative md:w-[50%]">
-              <Image src={boktips} layout="fill" objectFit="cover" />
+              <Image
+                src="/boktips.png"
+                alt="hej"
+                layout="fill"
+                objectFit="cover"
+              />
             </div>
 
             <div className="md:w-[50%] p-4 lg:flex flex-col justify-between">
