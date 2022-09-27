2dbc22d9dcefb3312ce49fd8e4d8d72ecd3981be
s0wie <sowieh@gmail.com>
2022-09-27 16:33:43 +0200

    tablet about page structure semi done

--- pages/about.js
+++ pages/about.js
@@ -10,13 +10,15 @@ export const About = (feed) => {
       <title>Majornas böcker & kaffe</title>
       <link rel="icon" href="/favicon.ico" />
       <h1 className="text-3xl font-h1 mb-4">OM OSS</h1>
-      <Image
-        src="/about-book.png"
-        width="343"
-        layout="responsive"
-        height="226"
-        alt="books"
-      ></Image>
+      <div className="md:hidden">
+        <Image
+          src="/about-book.png"
+          width="343"
+          layout="responsive"
+          height="226"
+          alt="books"
+        ></Image>
+      </div>
       <div className="w-full bg-temp-päron p-4 mt-4">
         <p className="font-bold mb-4">
           Oberoende bokhandel med med härligt kaffehäng!
@@ -29,36 +31,55 @@ export const About = (feed) => {
           per conubia nostra, per inceptos himenaeos.
         </p>
       </div>
-      <div className="bg-temp-ey mt-4 font-DMSerif text-[56px] text-hembakad leading-none p-4">
-        ALLTID HEMBAKAD FIKA TILL KAFFET
-        <div className="w-full text-center">
+      <div className="md:grid md:grid-cols-2">
+        <div>
+          <img
+            src="/about-book-2.png"
+            className="hidden md:block w-full mt-4 md:pr-4"
+          />
+        </div>
+        <div>
+          <div className="bg-temp-ey mt-4 font-DMSerif text-[56px] md:text-[32px] text-hembakad leading-none p-4">
+            ALLTID HEMBAKAD FIKA TILL KAFFET
+            <div className="w-full text-center">
+              <Image
+                src="/muffin-spoon.svg"
+                width="231"
+                height="218"
+                alt="muffin and spoon"
+              ></Image>
+            </div>
+          </div>
+          <div className="bg-tumbleweed mt-4 p-4 flex justify-center">
+            <div className="font-h1 underline mr-4">Hitta hit</div>
+            <div>&#8594;</div>
+          </div>
+        </div>
+      </div>
+      <div className="md:grid md:grid-cols-2 h-[300px] overflow-hidden">
+        <div className="bg-black-coffee text-whiteish mt-4 p-4">
+          <p className="mb-4 text-[26px]">
+            Eva Wadman{" "}
+            <span className="hidden md:inline">
+              är Majornas Bokhandel & Kaffe
+            </span>
+          </p>
+          <p>
+            Eva Wadman har alltid älskat litteraturens fascinerande värld. Den
+            förälskelsen växte sig så stark att 2019 öppnade hon sin egna
+            bokhandel i hjärtat av Majorna.
+          </p>
+        </div>
+        <div className="md:mt-4">
           <Image
-            src="/muffin-spoon.svg"
-            width="231"
-            height="218"
-            alt="muffin and spoon"
+            src="/about-eva.png"
+            layout="responsive"
+            width="343"
+            height="402"
+            alt="author"
           ></Image>
         </div>
       </div>
-      <div className="bg-tumbleweed mt-4 p-4 flex justify-center">
-        <div className="font-h1 underline mr-4">Hitta hit</div>
-        <div>&#8594;</div>
-      </div>
-      <div className="bg-black-coffee text-whiteish mt-4 font-h1 p-4">
-        <p className="mb-4 text-[26px]">Eva Wadman</p>
-        <p>
-          Eva Wadman har alltid älskat litteraturens fascinerande värld. Den
-          förälskelsen växte sig så stark att 2019 öppnade hon sin egna
-          bokhandel i hjärtat av Majorna.
-        </p>
-      </div>
-      <Image
-        src="/about-eva.png"
-        layout="responsive"
-        width="343"
-        height="402"
-        alt="author"
-      ></Image>
       <div className="mt-4 mb-4">Instagram</div>
       <Instagram></Instagram>
       <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
