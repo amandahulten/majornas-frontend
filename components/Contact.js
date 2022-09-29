12d2392d0b0580b26b7b8e334f8575ffdc1851d8
amandahulten <amahul1029@skola.goteborg.se>
2022-09-29 17:00:51 +0200

    Fixed instagram and google maps

--- components/Contact.js
+++ components/Contact.js
@@ -27,7 +27,7 @@ const Contact = ({ className, styling, display }) => {
         </div>
       </div>
 
-      <div className={`hidden md:flex gap-3 justify-end ${display}`}>
+      <div className={`hidden md:flex gap-3 justify-end pt-12 ${display}`}>
         <Link href="https://www.google.se/maps/place/Allm%C3%A4nna+v%C3%A4gen+22,+414+60+G%C3%B6teborg/@57.6969908,11.9282245,17z/data=!3m1!4b1!4m5!3m4!1s0x464ff3484c99eba3:0x7fa4782120b55e0!8m2!3d57.696988!4d11.9304078?hl=sv">
           <a
             className="text-lg underline font-['Courier-prime']"
